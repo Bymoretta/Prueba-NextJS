9706c648e637060e37df898579588858c726fb12
Bymoretta <axelmoretta22@gmail.com>
2022-06-12 20:05:59 -0300

    cambio 2

---
+++ pages/app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import Button from '@material-ui/core/button'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
