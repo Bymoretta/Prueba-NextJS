47cc5f85b4e2987a4e143bb052dfaddfcf7b0935
Bymoretta <axelmoretta22@gmail.com>
2022-06-12 21:40:54 -0300

    cambio 2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Cosmos from '../public/cosmos.gif'
-import sagan from '../public/Sagan.jpg'
+import Sagan from '../public/Sagan.jpg'
 import andromeda from '../public/andromeda.jpg'
 
 
@@ -27,7 +27,7 @@ export default function Home() {
         <h1>
           Carl Sagan
         </h1>
-        <img src='/mipagina/public/Sagan.jpg'/>
+        <img className={Sagan} src= {Sagan}alt='Sagan'/>
 
         <h2>
           Biografia
@@ -42,36 +42,7 @@ export default function Home() {
 
           A lo largo de su vida, Sagan recibió numerosos premios y condecoraciones por su labor como comunicador de la ciencia y la cultura. Hoy es considerado uno de los divulgadores de la ciencia más carismáticos e influyentes, gracias a su capacidad de transmitir las ideas científicas y los aspectos culturales al público no especializado con sencillez no exenta de rigor.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
 
       <footer className={styles.footer}>
